47e403070760764789b53ef45a18cef705f3d905
David-Chazoule <d.chazoule@gmail.com>
2025-03-04 20:49:29 +0100

    instalation, next,prisma,sass,axios and starting some component

---
+++ src/app/todo/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from "../../styles/todo.module.scss";
+
+function page() {
+  return (
+    <div className={styles.todoContainer}>page</div>
+  )
+}
+
+export default page
